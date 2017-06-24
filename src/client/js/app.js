cdb762960a1545dc5a180cdd5e8a3fc35b95d20b
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2017-06-24 15:15:05 -0400

    Comments

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,3 +1,8 @@
+/**
+ * Note that this is client code, but it still uses require! Webpack lets us do that, because it sees the
+ * dependencies and wires it all together when it builds our single client JS file.
+ */
+
 var Greeter = require('./greeter');
 
 (function(){
