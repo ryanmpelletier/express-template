ac61fc6341565b6cd6e6f195c7120daf919583a5
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2017-06-23 21:14:59 -0400

    Show how to add static resources accessible by html files served by express.

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+console.log(__dirname);
+//for allowing page to access static resources
+app.use('/js',express.static(path.join(__dirname, '../client/js')));
+app.use('/css',express.static(path.join(__dirname, '../client/css')));
+app.use('/img',express.static(path.join(__dirname, '../client/img')));
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'/../client/index.html'));
 });
